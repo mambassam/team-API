b81d344fe6066a5d7e90ea293b333906b9ffcfb7
lemmonas <lem20016@byui.edu>
2023-05-25 11:06:55 -0600

    Added Levi

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.get("/Lorenzo", (req, res)=> {res.send("Welcome Lorenzo!");
+app.get("/Lorenzo", (req, res)=> {res.send("Welcome Lorenzo!");});
 
-})
-app.get('/micah', (req,res) => {res.send("Hello Micah")});
+app.get('/micah', (req,res) => {res.send("Hello Micah");});
 
-app.get('/andra', (req,res) => {res.send("Hello Andra!")});
+app.get('/andra', (req,res) => {res.send("Hello Andra!");});
 
-app.get('/Moses', (req,res) => {res.send("Hello Moses!")});
+app.get('/Moses', (req,res) => {res.send("Hello Moses!");});
+
+app.get('/levi', (req,res) => {res.send("Hello Levi!");});
 
 app.listen(3000, ()=> {
     console.log("Listening");
