8e24efa6648d3f8c07ce71e6fce5d79c6e1c2fff
Lorenzo1400 <lorenzo.torres95@gmail.com>
2023-05-30 11:19:29 -0600

    1119

--- test/lorenzo.test.js
+++ test/lorenzo.test.js
@@ -1,27 +1,6 @@
-const response = await fetch('http://localhost:3000/Lorenzo');
-
-const name = await response.text();
-
 const fetch = require('node-fetch2')
-it ("Should test validity of token", async ()=> {
-    let token = "";
-    
-    const options = {
-        method: "POST",
-        headers:{ 
-            "content": "application/json"
-        
-        },
-        body:JSON.stringify({
-            userName:"lorenzo.torres95@gmail.com",
-            password :"Alberto1995!"
-        })
-    }
-    const response = await fetch("https://dev.stedi.me/login",options);
-    token = await response.text()
-    expect(token.length).toBe(36);
-    expect(name).toBe("Lorenzo");
+it ("Should get Lorenzo", async()=>{
+    const response = await fetch('http://localhost:3000/Lorenzo');
+    const name = await response.text();
+    expect(name).toBe("Hello Lorenzo!");
 });
-
-
-
